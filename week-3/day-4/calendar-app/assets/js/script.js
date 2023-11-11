3860030b3303cfa3f6561115f97f37304c7017cb
Stefania <galazzostefania@gmail.com>
2023-11-11 16:00:34 +0100

    add TOMBOLA + extra

--- week-3/day-4/calendar-app/assets/js/script.js
+++ week-3/day-4/calendar-app/assets/js/script.js
@@ -1,3 +1,106 @@
+//1. prendere il valore del mese corrente, scriverlo nell'h1,  e dei giorni del mese per generare le celle del calendario
+//
+const now = new Date(); // mi restituisce la data corrente
+console.log(now);
+
 // const getYear = new.getFullYear();
 // const getMonth = new.getMonth();
 // const lastDayDate = new Date(getYear, getMonth +1, 0)
+
+// for (let i = 1; i <= totalCells; i++) {
+//   const cell = document.createElement("div");
+//   cell.classList.add("cell");
+//   isCellEven = i % 2 === 0;
+// }
+
+//per salvare gli appuntamenti avrò bisogno di salvarli in un array
+//ogni appuntamento è una stringa
+//ogni giorno può avere diversi appuuntamenti
+//il numero dei mesi è variabile a seconda del mese
+
+/*
+es:
+[ array del mese
+    [day 1],[day 2],[day 3], ...
+]
+
+[ mese di novembre
+    ['appuntamento 1 di giorno 1','appuntamento 2 di giorno 1','appuntamento 3 di giorno 1',],
+    ['appuntamento 1 di giorno 2','appuntamento 2 di giorno 2','appuntamento 3 di giorno 2',],
+    [...],
+    [...],
+]
+*/
+// dentro array appointments pushiamo tanti sotto array quanti giorni del mese
+const appointments = [];
+const monthNames = [
+  "Gennaio",
+  "Febbraio",
+  "Marzo",
+  "Aprile",
+  "Marggio",
+  "Giugno",
+  "Luglio",
+  "Agosto",
+  "Settembre",
+  "Ottobre",
+  "Novembre",
+  "Dicembre",
+];
+
+const writeMonth = function () {
+  const title = document.querySelector("h1");
+  const monthIndex = now.getMonth(); //restituisce il numero del mese
+  const monthName = monthNames[monthIndex]; //prendo il nome del mese dall'array creato attraverso l'index
+  title.innerText = monthName;
+};
+writeMonth();
+
+// calcoliamo i giorni del mese per ogni mese
+const numberOfDays = function () {
+  const year = now.getFullYear();
+  const month = now.getMonth();
+  const lastDay = new Date(year, month + 1, 0); // è come se prendessi il valore dello 0 dicembre. ovvero l'ultimo giorno di novembre.
+  const dayNumber = lastDay.getDate();
+  console.log(dayNumber, lastDay, "numero dei gg", "last day");
+  return dayNumber;
+};
+numberOfDays();
+
+const deselectCells = function () {
+  const cells = document.querySelectorAll("day");
+  cells.forEach((el) => el.classList.remove("selected"));
+};
+
+const dayAppointment = function (dayIndx) {
+  const day = document.getElementById("newMeetingDay");
+  day.classList.add("hasDay");
+  day.innerText = dayIndx;
+};
+
+const createGrid = function (numDays) {
+  //giorni del mese corrente
+  const calendarDiv = document.getElementById("calendar");
+  for (i = 0; i > numDays; i++) {
+    const dayCell = document.createElement("div");
+    dayCell.classList.add("day");
+    dayCell.addEventListener("click", dayClickHandler(dayCell, i));
+    const cellValue = document.createElement("h4");
+    cellValue.innerText = i + 1;
+    calendarDiv.appendChild(dayCell);
+    cellValue.appendChild(cellValue);
+  }
+};
+createGrid(numberOfDays());
+
+function dayClickHandler(cell, i) {
+  console.log("dayclickhandler");
+  deselectCurrentCell();
+  cell.classList.add("selected");
+  dayAppointment(i);
+  if (appointments[i].length > 0) showAppointments(i);
+  else {
+    const divAppointmets = document.getElementById("appointments");
+    divAppointmets.style.display = "none";
+  }
+}
---
+++ week-3/day-4/tombola/assets/js/script.js
@@ -0,0 +1,130 @@
+const stratBTN = document.getElementById("start");
+const myCardBTN = document.getElementById("chooseNumToPlay");
+const extractionBTN = document.getElementById("extract");
+const extractNumbers = [];
+stratBTN.addEventListener("click", init);
+document.addEventListener("click", checkStartGame);
+myCardBTN.addEventListener("click", createCard);
+extractionBTN.addEventListener("click", function () {
+  extraction();
+}); // estraggo i numeri casuali
+
+function checkStartGame() {
+  if (extractionBTN.disabled) {
+    const input = document.getElementById("choosenCard");
+    input.disabled = true;
+    document.querySelector("#start").innerHTML = "Start the game!";
+  }
+}
+function init() {
+  const input = document.getElementById("choosenCard");
+  input.disabled = false;
+  const totalCells = 90;
+  const bingoBoard = document.getElementById("bingoBoard");
+  for (let i = 0; i < totalCells; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.innerHTML = i + 1;
+    bingoBoard.appendChild(cell);
+  }
+  stratBTN.style.display = "none";
+  extractionBTN.disabled = false;
+  createCard();
+}
+
+function extraction() {
+  //funzione ricorsiva: una funziona che a una data condizione verificata richiama se stessa
+  let extractedNumber = document.getElementById("extractedNumber");
+  let randomNum = Math.floor(Math.random() * 91);
+  let invalidNum = extractNumbers.find((n) => n === randomNum);
+  if (invalidNum || randomNum === 0) {
+    extraction();
+  } else {
+    extractNumbers.push(randomNum);
+    extractedNumber.innerHTML = randomNum;
+    colorCell(randomNum);
+    colorCardCell(randomNum);
+  }
+  youWin();
+}
+
+function colorCell(n) {
+  const cells = document.querySelectorAll(".cell");
+  cells.forEach((el) => {
+    //confronto il num interno alla cella con il numero estratto
+    let numCell = Number(el.innerText);
+    if (numCell === n) {
+      el.style.backgroundColor = "rgb(188, 134, 255)";
+      el.style.color = "#fff";
+    }
+  });
+}
+function colorCardCell(n) {
+  const cardCells = document.querySelectorAll(".cardCell");
+  cardCells.forEach((el) => {
+    //confronto il num interno alla cella con il numero estratto
+    let numCell = Number(el.innerText);
+    if (numCell === n) {
+      el.classList.add("extracted");
+    }
+  });
+}
+
+function createCard() {
+  const cardContainer = document.querySelector("#userCard");
+  const input = document.getElementById("choosenCard");
+  let numbersInCard = [];
+  let myValues = [];
+  let myValue = input.value;
+  myValues.push(Number(myValue));
+  input.value = "";
+  for (let i = 0; i < myValues[0]; i++) {
+    let error = document.querySelector("#error");
+    if (myValues[0] >= input.min && myValues[0] <= input.max) {
+      let randomNumCard = Math.floor(Math.random() * 91);
+      numbersInCard.push(randomNumCard);
+      const cardCell = document.createElement("div");
+      cardCell.classList.add("cardCell");
+      cardCell.innerHTML = `<p>${numbersInCard[i]}</p>`;
+      cardContainer.appendChild(cardCell);
+      input.disabled = true;
+      error.innerHTML = "";
+    } else {
+      error.innerHTML = "Scegli i numeri per la tua cartella!";
+    }
+  }
+}
+
+function youWin() {
+  const cardCells = document.querySelectorAll(".cardCell");
+  const extractedCardCells = document.querySelectorAll(".extracted");
+  let winScreen = document.querySelector("#winScreen");
+  if (cardCells.length === extractedCardCells.length)
+    setTimeout((winScreen.style.display = "block"), 1000);
+  playAgain();
+}
+
+function playAgain() {
+  const reStartBTN = document.getElementById("re-start");
+  reStartBTN.onclick = () => {
+    location.reload();
+  };
+}
+// function showError() {
+//   const input = document.getElementById("choosenCard");
+//   const error = document.querySelector("#error");
+//   if (input.checkValidity())
+//     error.innerHTML = "Scegli i numeri per la tua cartella!";
+//   else if (input.disabled)
+//     error.innerHTML =
+//       "Hai già scelto! Per fare una nuova scelta inizia un'altra partita";
+// }
+
+// function chooseCardNums() {
+//   const input = document.querySelector("input#choosenCard");
+//   let myValues = [];
+//   let value = input.value;
+//   myValues.push(value);
+//   console.log(input.value, "input value");
+//   console.log(myValues, "myvaluesssssss");
+// }
