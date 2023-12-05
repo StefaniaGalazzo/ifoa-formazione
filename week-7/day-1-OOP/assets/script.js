7bc8ee09e831cc497c1d9494e2bebbb6ecc314cd
Stefania <galazzostefania@gmail.com>
2023-12-05 15:35:04 +0100

    add week 7 day 1

---
+++ week-7/day-1-OOP/assets/script.js
@@ -0,0 +1,153 @@
+/***** CONFRONTO TRA DUE UTENTI  *****/
+// class User {
+//   constructor(firstName, lastName, age, location) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.age = age;
+//     this.location = location;
+//   }
+
+//   compareAge(altraPersona) {
+//     if (this.age > altraPersona.age) {
+//       return `${this.firstName} è più vecchio di ${altraPersona.firstName}`;
+//     } else if (this.age < altraPersona.age) {
+//       return `${this.firstName} è più giovane di ${altraPersona.firstName}`;
+//     } else {
+//       return `${this.firstName} ha la stessa età di ${altraPersona.firstName}`;
+//     }
+//   }
+// }
+
+/***** CONFRONTO TRA PIU UTENTI  *****/
+// class User {
+//   constructor(firstName, lastName, age, location) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.age = age;
+//     this.location = location;
+//   }
+
+//   confrontaEta(altrePersone) {
+//     if (!Array.isArray(altrePersone) || altrePersone.length === 0) {
+//       return "Devi fornire un array di almeno due utenti per il confronto.";
+//     }
+
+//     const risultatiConfronto = altrePersone.map((altraPersona) => {
+//       if (this.age > altraPersona.age) {
+//         return `${this.firstName} è più vecchio di ${altraPersona.firstName}`;
+//       } else if (this.age < altraPersona.age) {
+//         return `${this.firstName} è più giovane di ${altraPersona.firstName}`;
+//       } else if (this.firstName != altraPersona.firstName) {
+//         return `${this.firstName} ha la stessa età di ${altraPersona.firstName}`;
+//       }
+//     });
+
+//     return risultatiConfronto.join("\n"); // Unisce gli elementi dell'array in una sola stringa
+//   }
+// }
+
+class User {
+  constructor(firstName, lastName, age, location) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.location = location;
+  }
+
+  compareAge(altrePersone) {
+    if (!Array.isArray(altrePersone) || altrePersone.length === 0) {
+      return "Devi fornire un array di almeno due utenti per il confronto.";
+    }
+
+    let risultato = "";
+    for (let i = 0; i < altrePersone.length - 1; i++) {
+      const personaA = altrePersone[i];
+      for (let j = i + 1; j < altrePersone.length; j++) {
+        const personaB = altrePersone[j];
+        if (personaA.age > personaB.age) {
+          risultato += `${personaA.firstName} è più vecchio di ${personaB.firstName}\n`;
+        } else if (personaA.age < personaB.age) {
+          risultato += `${personaA.firstName} è più giovane di ${personaB.firstName}\n`;
+        } else {
+          risultato += `${personaA.firstName} ha la stessa età di ${personaB.firstName}\n`;
+        }
+      }
+    }
+
+    return risultato.trim(); // Rimuove eventuali spazi bianchi extra alla fine
+  }
+}
+// uso la classe User per creare due utenti
+const utente2 = new User("Stefania", "Galazzo", 33, "Torino");
+const utente1 = new User("Marco", "Furi", 30, "Milano");
+const utente3 = new User("Alice", "Zarattani", 31, "Bologna");
+
+// Confronto delle età e stampa del risultato
+const risultatoConfronto = utente1.compareAge([utente1, utente2, utente3]);
+console.log(risultatoConfronto);
+
+//
+/******* Form Pet *******/
+const submitBtn = document.getElementById("createPet");
+const petName = document.getElementById("petName");
+const ownerName = document.getElementById("ownerName");
+const species = document.getElementById("species");
+const breed = document.getElementById("breed");
+const petsList = document.getElementById("listaPets");
+
+const petsArray = [];
+
+submitBtn.addEventListener("click", function () {
+  if (
+    petName.value != "" &&
+    ownerName.value != "" &&
+    species.value != "" &&
+    breed.value != ""
+  ) {
+    addPet();
+    printPets();
+    resettaForm();
+  } else {
+    document.querySelector(
+      "ul#listaPets"
+    ).innerHTML = `<p class='error'> Inserisci i dati per il tuo Pet</p>`;
+  }
+});
+
+class Pet {
+  constructor(petName, ownerName, species, breed) {
+    this.petName = petName;
+    this.ownerName = ownerName;
+    this.species = species;
+    this.breed = breed;
+  }
+  condividePadrone(altraPet) {
+    return this.ownerName === altraPet.ownerName;
+  }
+}
+
+// aggiungo all'array i valori presi dal form
+function addPet() {
+  const nuovaPet = new Pet(
+    petName.value,
+    ownerName.value,
+    species.value,
+    breed.value
+  );
+  petsArray.push(nuovaPet);
+  console.log(petsArray, "petsArray");
+}
+
+// stampo i valori del form nella lista html
+function printPets() {
+  console.log(petsList, "petsList");
+  petsList.innerHTML = "";
+  petsArray.forEach((pet) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `Pet Name: ${pet.petName}, Owner Name: ${pet.ownerName}, Species: ${pet.species}, Breed: ${pet.breed}`;
+    petsList.appendChild(listItem);
+  });
+}
+function resettaForm() {
+  document.getElementById("petForm").reset();
+}
