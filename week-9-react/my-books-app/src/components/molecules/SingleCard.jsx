97f5c80ba0ddd5c2a227c8ea69e7ab339e62ebfe
Stefania <galazzostefania@gmail.com>
2024-01-10 18:23:31 +0100

    fix e upd style card border

--- week-9-react/my-books-app/src/components/molecules/SingleCard.jsx
+++ week-9-react/my-books-app/src/components/molecules/SingleCard.jsx
@@ -1,9 +1,20 @@
 /* eslint-disable react/prop-types */
 import { Button, Card } from "react-bootstrap";
+import { useState } from "react";
 
 export default function SingleCard({ book }) {
+  const [clicked, setClicked] = useState(false);
+
+  const clickHandler = () => {
+    setClicked(!clicked);
+  };
+
   return (
-    <Card className="m-2" style={{ width: "18rem" }}>
+    <Card
+      className="m-2"
+      style={{ width: "18rem", border: clicked ? "3px solid orange" : "" }}
+      onClick={() => clickHandler()}
+    >
       <Card.Img variant="top" src={book.img} />
       <Card.Body>
         <Card.Title>{book.title}</Card.Title>
--- week-9-react/my-books-app/src/components/templates/CardsCategoriesTemplate.jsx
+++ week-9-react/my-books-app/src/components/templates/CardsCategoriesTemplate.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import ButtonsCategories from "../organisms/ButtonsCategories";
 import CardsCustom from "../organisms/CardsCustom";
@@ -17,25 +17,56 @@ export default function CardsCategoriesTemplate() {
   const handleSearch = (e) => {
     let term = e.target.value;
     setSearchTerm(term);
-    // prevCate è una callback
-    setCategory([...fantasy, ...horror, ...romance, ...scifi, ...history]);
-    setCategory((prevCategory) => {
-      return prevCategory.filter((book) =>
-        book.title.toLowerCase().includes(term.toLowerCase())
-      );
-    });
-    setMessage(category.length === 0);
+    const filteredCategory = [
+      ...fantasy,
+      ...horror,
+      ...romance,
+      ...scifi,
+      ...history,
+    ].filter((book) => book.title.toLowerCase().includes(term.toLowerCase()));
+
+    setCategory(filteredCategory);
+    setMessage(filteredCategory.length === 0);
   };
+
+  useEffect(() => {
+    debounce(handleSearch, 800);
+  }, [category.length, searchTerm]);
+
+  // vecchia hanlder search basic
+  // const handleSearch = (e) => {
+  //   let term = e.target.value;
+  //   setSearchTerm(term);
+  //   // prevCate è una callback
+  //   setCategory([...fantasy, ...horror, ...romance, ...scifi, ...history]);
+  //   setCategory((prevCategory) => {
+  //     return prevCategory.filter((book) =>
+  //       book.title.toLowerCase().includes(term.toLowerCase())
+  //     );
+  //   });
+  //   console.log(category, "categori in hadlesearch");
+  //   console.log(category.length === 0, "category.length === 0");
+  //   setMessage(category.length === 0);
+  // };
 
   return (
     <Container fluid className="mb-5 pb-5">
       <SearchBar val={searchTerm} handler={handleSearch} wdt="300px" />
       <ButtonsCategories setCategory={setCategory} />
-      {category && <CardsCustom category={category} />}
+      {category.length !== 0 && <CardsCustom category={category} />}
       {category.length === 0 && !message && (
         <h3 className="text-center">Search a book or select a category!</h3>
       )}
       {message && <h3 className="text-center">There are no results</h3>}
     </Container>
   );
+
+  // Funzione di debounce
+  function debounce(func, delay) {
+    let timeoutId;
+    return function (...args) {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => func(...args), delay);
+    };
+  }
 }
